34916b3f30b70982bba656eae9cbc01ff2c417fe
zhouxinle <zhouxinle2012@163.com>
2014-04-23 15:34:58 +0800

    0423

---
+++ InstgramBeta/InstgramBeta/InstgramAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  InstgramAppDelegate.h
+//  InstgramBeta
+//
+//  Created by zhouxinle on 14-4-23.
+//  Copyright (c) 2014年 imac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstgramAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InstgramBeta/InstgramBeta/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InstgramBeta
+//
+//  Created by zhouxinle on 14-4-23.
+//  Copyright (c) 2014年 imac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "InstgramAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([InstgramAppDelegate class]));
+    }
+}
